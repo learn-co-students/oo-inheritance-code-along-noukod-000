b662305286d73b0b53284f40d5b1f7d75dc1db9e
Saint-Jean Germila <germila@noukod.com>
2018-10-18 16:07:02 +0000

    Done.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,17 +1,17 @@
 class Vehicle
 
-attr_accessor :wheel_size : wheel_number
+  attr_accessor :wheel_size, :wheel_number
 
   def initialize(wheel_size, wheel_number)
- @wheel_size = wheel_size
- @wheel_number = wheel_number
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
   end
 
   def go
-   "vrrrrrrrooom!"
-   end
+    "vrrrrrrrooom!"
+  end
 
-   def fill_up_tank
+  def fill_up_tank
     "filling up!"
   end
 
